4ebd858e4e8daf1076f44381c1e1e00758058ca5
Kate Bennert <kate.bennert@gmail.com>
2022-06-09 14:55:35 -0400

    commit tree c57e820730617125d58813e8234c7d3e8b4a643f

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const hQLocation = 42;
+const feetInBlock = 264;
+
+function distanceFromHqInBlocks(start) {
+    return Math.abs(hQLocation - start);
+}
+
+function distanceFromHqInFeet(start) {
+    return distanceFromHqInBlocks(start) * feetInBlock;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * feetInBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return ((distanceTravelledInFeet(start, destination) - 400) * 2) / 100;
+    } else if (distanceFromHqInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
